a538cd66c25588a20a61f14cb6765c1988d50331
Stephen Kuenzli <skuenzli@qualimente.com>
2016-07-23 15:20:06 -0700

    Create a large network for each availability zone.

--- terraform/aws/vpc/main.tf
+++ terraform/aws/vpc/main.tf
@@ -1,8 +1,20 @@
 # Define a Virtual Private Cloud and required networks to host the infrastructure
 
-variable "name" {default = "docker-infra-quickstart"}
-variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "name" { default = "docker-infra-quickstart" }
+variable "vpc_cidr" { default = "10.42.0.0/16" }
 variable "region" {}
+variable "availability_zones"  {  default = "a,b,c" }
+
+# https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
+variable "cidr_blocks" {
+  default = {
+    az0 = "10.42.0.0/18"
+    az1 = "10.42.64.0/18"
+    az2 = "10.42.128.0/18"
+    #az3 = "10.42.192.0/18" # could add a fourth az, if region permits
+  }
+}
+
 
 resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr}"
@@ -10,5 +22,15 @@ resource "aws_vpc" "main" {
   tags {
     Name = "${var.name}"
     CreatedBy = "terraform"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  count = "${length(split(",", var.availability_zones))}"
+  cidr_block = "${lookup(var.cidr_blocks, concat("az", count.index))}"
+  availability_zone = "${var.region}${element(split(",", var.availability_zones), count.index)}"
+  tags {
+    Name = "${concat("az", count.index)}"
   }
 }
