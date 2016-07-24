9f9be6ba4f9bc4d059bb247d3db086173393d646
Stephen Kuenzli <skuenzli@qualimente.com>
2016-07-23 19:38:48 -0700

    Add a file to define VPC-wide security groups.

---
+++ terraform/aws/vpc/security-groups.tf
@@ -0,0 +1,18 @@
+variable "name" {}
+
+resource "aws_security_group" "bastion" {
+  name = "${var.name}-bastion"
+  description = "Inbound traffic for ${var.name} bastion nodes"
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    CreatedBy = "terraform"
+  }
+
+  ingress { # SSH
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
