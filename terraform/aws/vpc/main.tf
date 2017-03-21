4ebf654a3ac455ea19f82951eb93a70586adb821
Stephen Kuenzli <skuenzli@qualimente.com>
2017-03-20 22:03:26 -0700

    Start migrating subnet definitions into vpc module

--- terraform/aws/vpc/main.tf
+++ terraform/aws/vpc/main.tf
@@ -1,15 +1,53 @@
 # Define a Virtual Private Cloud and required networks to host the infrastructure
 
 variable "name" { default = "docker-infra-quickstart" }
+variable "environment" { }
 variable "cidr" { default = "10.42.0.0/16" }
 variable "region" {}
+variable "az_cidr_blocks" {
+  type = "map"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "${var.cidr}"
+  enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags {
     Name = "${var.name}"
     CreatedBy = "terraform"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name        = "${var.name}"
+    Environment = "${var.environment}"
+  }
+}
+
+
+module "dmz_subnet" {
+  source = "github.com/terraform-community-modules/tf_aws_public_subnet"
+
+  name   = "${var.name}-dmz"
+
+  cidrs = [
+      "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 0)), 6, 0)}"
+      , "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 1)), 6, 0)}"
+      , "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 2)), 6, 0)}"
+    ]
+
+  azs    = "${keys(var.az_cidr_blocks)}"
+
+  vpc_id = "${aws_vpc.main.id}"
+  igw_id = "${aws_internet_gateway.main.id}"
+
+  tags {
+      CreatedBy = "terraform"
+      Environment = "${var.environment}"
   }
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,30 @@ provider "aws" {
   region = "${var.region}"
 }
 
-variable "region" {default = "us-west-2"}
 variable "name" {default = "docker-infra-quickstart"}
+variable "environment" {
+  description = "Environment tag, recommendation: <account name>_<region>, e.g. prod_us-west-2"
+  default     = "qm_us-west-2"
+}
+
+variable "region" {default = "us-west-2"}
+variable "availability_zones" {
+  description = "A comma-separated list of availability zones"
+  type        = "list"
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
 
 # Define CIDR blocks for the VPC and Availability Zones within the VPC
 # https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
 # A key network design goal is to enable the AZ to a significant number of c-blocks to support network segmentation
 # Providing a /18 network to the AZ enables further division of up to 64 c-block-sized subnets
 variable "vpc_cidr" {default = "10.42.0.0/16"}
-variable "cidr_blocks" {
+variable "az_cidr_blocks" {
   type = "map"
   default = {
-    az0 = "10.42.0.0/18"
-    az1 = "10.42.64.0/18"
-    az2 = "10.42.128.0/18"
+    "us-west-2a" = "10.42.0.0/18"
+    "us-west-2b" = "10.42.64.0/18"
+    "us-west-2c" = "10.42.128.0/18"
     #az3 = "10.42.192.0/18" # could add a fourth az, if region permits
   }
 }
@@ -23,41 +33,12 @@ variable "cidr_blocks" {
 module "vpc" {
   source = "./aws/vpc"
   name = "${var.name}"
+  environment = "${var.environment}"
   region = "${var.region}"
   cidr = "10.42.0.0/16"
+  az_cidr_blocks = "${var.az_cidr_blocks}"
 }
 
 module "keypairs" {
   source ="./aws/keypairs"
-}
-
-# Define 3 availability zones
-module "az0" {
-  source ="./aws/az"
-  region = "${var.region}"
-  vpc_id = "${module.vpc.vpc_id}"
-  az_id = "${var.region}a"
-  az_cidr = "${lookup(var.cidr_blocks, "az0")}"
-
-  default_security_group_id = "${module.vpc.default_security_group_id}"
-}
-
-module "az1" {
-  source ="./aws/az"
-  region = "${var.region}"
-  vpc_id = "${module.vpc.vpc_id}"
-  az_id = "${var.region}b"
-  az_cidr = "${lookup(var.cidr_blocks, "az1")}"
-
-  default_security_group_id = "${module.vpc.default_security_group_id}"
-}
-
-module "az2" {
-  source ="./aws/az"
-  region = "${var.region}"
-  vpc_id = "${module.vpc.vpc_id}"
-  az_id = "${var.region}c"
-  az_cidr = "${lookup(var.cidr_blocks, "az2")}"
-
-  default_security_group_id = "${module.vpc.default_security_group_id}"
 }
