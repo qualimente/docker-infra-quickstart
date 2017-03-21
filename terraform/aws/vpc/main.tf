6d7ef0e475593686dbfb9ca983d81bb405f24ecc
Stephen Kuenzli <skuenzli@qualimente.com>
2017-03-20 22:29:00 -0700

    Add a private app subnet.

--- terraform/aws/vpc/main.tf
+++ terraform/aws/vpc/main.tf
@@ -28,11 +28,23 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_eip" "nat" {
+  count = "3"
+  vpc   = true
+  depends_on    = ["module.dmz_subnet"]
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = "3"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(module.dmz_subnet.subnet_ids, count.index)}"
+  depends_on    = ["aws_internet_gateway.main", "module.dmz_subnet"]
+}
 
 module "dmz_subnet" {
   source = "github.com/terraform-community-modules/tf_aws_public_subnet"
 
-  name   = "${var.name}-dmz"
+  name   = "${var.environment}-dmz"
 
   cidrs = [
       "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 0)), 6, 0)}"
@@ -49,6 +61,26 @@ module "dmz_subnet" {
       CreatedBy = "terraform"
       Environment = "${var.environment}"
   }
+}
+
+module "app_subnet" {
+  source = "github.com/terraform-community-modules/tf_aws_private_subnet_nat_gateway"
+
+  name   = "${var.environment}-app"
+
+  cidrs = [
+      "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 0)), 6, 1)}"
+      , "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 1)), 6, 1)}"
+      , "${cidrsubnet(lookup(var.az_cidr_blocks, element(keys(var.az_cidr_blocks), 2)), 6, 1)}"
+    ]
+
+  azs    = "${keys(var.az_cidr_blocks)}"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  nat_gateways_count = "1"
+  public_subnet_ids  = "${module.dmz_subnet.subnet_ids}"
+
 }
 
 output "vpc_id" {
